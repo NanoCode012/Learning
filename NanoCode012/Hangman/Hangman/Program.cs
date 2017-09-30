cbfd306a0449db63d6f78d6da834b76267874899
NanoCode012 <kevinvong@rocketmail.com>
2017-10-01 00:52:53 +0700

    Revamped the whole project
    Made it more clean hopefully.
    Broke down code into smaller methods.
    Added another feature to detect duplicate entries.

--- NanoCode012/Hangman/Hangman/Program.cs
+++ NanoCode012/Hangman/Hangman/Program.cs
@@ -8,6 +8,7 @@ namespace Hangman
     {
         public static void Main(string[] args)
         {
+            #region list
             var list = new List<string>
             {
                 "happy",
@@ -27,81 +28,134 @@ namespace Hangman
                 "durian",
                 "lychee"
             };
+            #endregion
 
-            var random = new Random();
-            var index = random.Next(0, list.Count);
+            //Declaraction
+            var index = new Random().Next(0, list.Count);
             var word = new StringBuilder(list[index]);
-            var wordLength = word.Length;
             var spots = new StringBuilder("");
             var lives = 3;
-            for (var i = 0; i < wordLength; i++)
-            {
-                spots.Append("_"); 
-            }
+            var nonDuplicateList = new List<string>();
+			ChangeCharsToHyphens(word, spots);
 
-            Console.WriteLine("===============================");
+            Console.WriteLine("==========================================");
+
             //Lives control
             Console.WriteLine("Please choose how many one-digit extra lives you want(default is 3): ");
             var tempLivesString = Console.ReadLine();
-            if (tempLivesString.Length == 1 && int.TryParse(tempLivesString, out int tempLivesInteger)) 
+            lives = ValidateSetLives(lives, tempLivesString);
+
+            //MainGame
+            Console.WriteLine("Guess my word!! : {0} with {1} extra lives left", spots, lives);
+            while (spots.ToString().Contains("_") && lives >= 0)
+            {
+                string pressedLetter = GetGuessedLetter(nonDuplicateList);
+                if (word.ToString().Contains(pressedLetter))
+                {
+                    Console.WriteLine("You got one!");
+                    ReplaceWordWithZero(word, spots, pressedLetter);
+                    AddNonDupeToList(word, nonDuplicateList, pressedLetter);
+                }
+                else
+                {
+                    nonDuplicateList.Add(pressedLetter);
+                    lives--;
+                    if (lives < 0) break;
+					Console.WriteLine("Oops. That's not right... You have {0} extra lives left", lives);
+					continue;
+                }
+                Console.WriteLine("So far you've got : {0} with {1} extra lives left", spots, lives);
+            }
+            LastWords(spots, lives);
+        }
+
+        static void ReplaceWordWithZero(StringBuilder word, StringBuilder spots, string pressedLetter)
+        {
+            var tempIndex = word.ToString().IndexOf(pressedLetter, StringComparison.Ordinal);
+            word[tempIndex] = '0';
+            spots[tempIndex] = Convert.ToChar(pressedLetter);
+        }
+
+        static void AddNonDupeToList(StringBuilder word, List<string> nonDuplicateList, string pressedLetter)
+        {
+            if (!word.ToString().Contains(pressedLetter))
+            {
+                nonDuplicateList.Add(pressedLetter);
+            }
+        }
+
+        static string GetGuessedLetter(List<string> nonDuplicateList)
+        {
+            var pressedLetter = "";
+            while (true)//defense against more than one char pressed
+            {
+                Console.WriteLine("Please pick a letter.");
+                pressedLetter = Console.ReadLine().ToLower();//defense against CAPS
+                if (ValidateInput(pressedLetter, nonDuplicateList)) break;
+            }
+
+            return pressedLetter;
+        }
+
+        static bool ValidateInput(string pressedLetter, List<string> nonDupeList)
+		{
+			var check = false;
+			if (pressedLetter.Length != 1)
+			{
+				Console.WriteLine("Error. Please enter only one character. Try again!");
+			}
+			else if ((Convert.ToChar(pressedLetter)) < 'a' || (Convert.ToChar(pressedLetter)) > 'z')//only allow a-z
+			{
+				Console.WriteLine("Error. Please enter only alphabetical character. Do it again!");
+			}
+			else if (nonDupeList.Contains(pressedLetter))
+			{
+				Console.WriteLine("You've already picked that number. Pick another instead!");
+			}
+			else
+			{
+				check = true;
+			}
+			return check;
+		}
+
+        static int ValidateSetLives(int lives, string tempLivesString)
+        {
+            if (tempLivesString.Length == 1 && int.TryParse(tempLivesString, out int tempLivesInteger))
             {
                 if (tempLivesInteger == 0)
                 {
                     Console.WriteLine("So Daring!! You can't make any mistake!");
                 }
-				lives = tempLivesInteger;
+                lives = tempLivesInteger;
             }
             else
             {
                 Console.WriteLine("You entered an invalid value. Lives set to default.");
             }
 
-            //MainGame
-			Console.WriteLine("Guess my word!! : {0} with {1} extra lives left",spots, lives);
-            while (spots.ToString().Contains("_") && lives >= 0)
-            {            
-				var pressedLetter = "";
-				while(true)//defense against more than one char pressed
-				{
-					Console.WriteLine("Please pick a letter.");
-                    pressedLetter = Console.ReadLine().ToLower();//todo check if pressed before//cannot do this as it will 
-                                                                //clash with duplicate characters method below
-                    if (pressedLetter.Length != 1 )
-                    {
-                        Console.WriteLine("Error. Please enter only one character. Try again!");
-                    }else if ((Convert.ToChar(pressedLetter)) < 'a' ||  (Convert.ToChar(pressedLetter)) > 'z')//only allow a-z
-                    {
-                        Console.WriteLine("Error. Please enter only alphabetical character. Do it again!");
-                    }else
-                    {
-						break;
-                    }
-				}
+            return lives;
+        }
 
-				if (word.ToString().Contains(pressedLetter))
-				{
-                    Console.WriteLine("You got one!");
-                    var tempIndex = word.ToString().IndexOf(pressedLetter, StringComparison.Ordinal);
-                    word[tempIndex] = '0';
-                    spots[tempIndex] = Convert.ToChar(pressedLetter);
-
-                }else {
-                    lives--;
-                    if (lives < 0) break;
-					Console.WriteLine("oops. That's not right... You have {0} extra lives left",lives);
-                    continue;
-				}
-                Console.WriteLine("So far you got : {0} with {1} extra lives left",spots,lives);
-            }
-            if (lives > 0)
+        static void ChangeCharsToHyphens(StringBuilder word, StringBuilder spots)
+        {
+            for (var i = 0; i < word.Length; i++)
             {
-				Console.WriteLine("Congrats you beat this game!");
-            }else
+                spots.Append("_");
+            }
+        }
+
+        static void LastWords(StringBuilder spots, int lives)
+        {
+            Console.WriteLine();
+            if (lives >= 0)
+            {
+                Console.WriteLine("Congrats you beat this game! The word was {0}! Hurray!", spots);
+            }
+            else
             {
                 Console.WriteLine("Bad game? Try again. Reopen app");
             }
-
-
         }
     }
 }
