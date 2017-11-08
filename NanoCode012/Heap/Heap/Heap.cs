6497910d1577250fb1affc9bc93ed8533e9882cf
NanoCode012 <kevinvong@rocketmail.com>
2017-11-08 20:41:29 +0700

    Added new Project
    Heap is a data structure built from Lists, that has the last index as the largest value in the list.

---
+++ NanoCode012/Heap/Heap/Heap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Heap
+{
+    public class Heap<T> where T : IComparable
+    {
+        readonly List<T> list = new List<T>();
+
+        public void Add(T val){
+			int lastIndex = list.Count - 1;
+            if (lastIndex == -1){
+                list.Add(val);
+            }else{            
+				if (list[lastIndex].CompareTo(val) <= 0){//num is bigger
+                    list.Add(val);
+                }else if (list[lastIndex].CompareTo(val) > 0){
+					var temp = list[lastIndex];
+					list[lastIndex] = val;
+                    list.Add(temp);
+				}
+            }
+        }
+
+        public T Peek(){
+            return list[list.Count - 1];
+        }
+
+        public T Pop(){
+            var temp = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return temp;
+        }
+
+    }
+}
---
+++ NanoCode012/Heap/Heap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Heap
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var h = new Heap<int>();
+            h.Add(5);
+            h.Add(7);
+            h.Add(2);
+            h.Add(8);
+            Console.WriteLine(h.Peek());
+            Console.WriteLine(h.Pop());
+            Console.WriteLine(h.Pop());
+            Console.WriteLine(h.Pop());
+            Console.WriteLine(h.Pop());
+        }
+    }
+}
